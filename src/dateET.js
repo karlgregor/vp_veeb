aec6ba3c02eaab79b51a8f4eebbb41f0c77d71d0
karlgregor <keerleskarlgregor@gmail.com>
2025-09-30 11:22:20 +0300

    src + public

---
+++ src/dateET.js
@@ -0,0 +1,6 @@
+//function dateFormattedET(){
+    exports.dateFormattedET = function(){
+        let timeNow = new Date();
+        const monthNamesET = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+        return timeNow.getDate() + ". " + monthNamesET[timeNow.getMonth()] + " " + timeNow.getFullYear();
+    }
