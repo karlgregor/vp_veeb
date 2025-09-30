972b352ed31f495b2b1873fbb6e06274a527a431
karlgregor <keerleskarlgregor@gmail.com>
2025-09-30 11:51:03 +0300

    ejs + style

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 2125
+const port = 5221
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
+})
+
+app.get('/test', (req, res) => {
+  res.render('test', { 
+    title: 'test title',
+    message: 'Hello from Express!',
+    items: ['item1', 'item2', 'item3']
+  })
 })
 
 app.listen(port, () => {
