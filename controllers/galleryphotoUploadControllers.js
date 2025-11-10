400a2f400368d3e32e5eb42e626656fda0f4ea0f
karlgregor <keerleskarlgregor@gmail.com>
2025-11-11 00:30:51 +0200

    vesimärgi parandus

--- controllers/galleryphotoUploadControllers.js
+++ controllers/galleryphotoUploadControllers.js
@@ -2,7 +2,6 @@ const mysql = require('mysql2/promise')
 const fs = require("fs").promises;
 const sharp = require("sharp");
 const dbInfo = require('../dbInfo')
-const fs = require("fs").promises;
 
 console.log("Exists:", await fs.exists("./public/images/test.png"));
 
