a421ef64c1aed7ce3bd51783a52ff9f60e86c280
karlgregor <keerleskarlgregor@gmail.com>
2025-11-25 12:55:40 +0200

    personal picture viewer

--- routes/photogalleryRoutes.js
+++ routes/photogalleryRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const loginCheck = require("../src/loginMiddleware");
 const {photogalleryHome, photogalleryPersonalHome, photogalleryPersonalPic, photogalleryPersonalPicPost} = require("../controllers/photoGalleryControllers");
+
+router.use(loginCheck.isLogin);
 
 router.route("/").get(photogalleryHome);
 router.route("/myphotos").get(photogalleryPersonalHome);
-router.route("/myphotos/:id").get(photogalleryPersonalPic).get(photogalleryPersonalPicPost);
-
+router.route("/myphotos/:id")
+  .get(photogalleryPersonalPic)
+  .post(photogalleryPersonalPicPost);
 	
 module.exports = router;
